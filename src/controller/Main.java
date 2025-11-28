c13547fa8110fb22ff650b7a4fd821c7353f87f7
IbbeTwo <ibrahimkhaledtwo@gmail.com>
2025-11-28 10:59:59 +0100

    Update Main.java

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -4,7 +4,5 @@ public class Main {
     public static void main(String[] args) {
         Controller theController = new Controller();
 
-        System.out.println("eafea");
-        System.out.println("fad");
     }
 }
