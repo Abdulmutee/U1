ab47cc7da591a72173f11b6407210028004508a1
Abdulmutee <abdulmoteesh@gmail.com>
2025-12-01 15:03:11 +0100

    Detta gjorde vi tillsammans

---
+++ src/model/Order.java
@@ -0,0 +1,33 @@
+package model;
+
+public class Order {
+
+    private Products[] products;
+
+    public Order() {
+        this.products = new Products[10];
+    }
+
+    // Tillfälligt
+    public int totalPrice() {
+        return 0;
+    }
+
+    public void addProduct(Products product) {
+        for (int i = 0; i< this.products.length; i++) {
+            if (this.products[i] == null) {
+                this.products[i] = product;
+                return;
+            }
+        }
+        Products[] newList = new Products[this.products.length+5];
+
+        for (int i = 0; i< this.products.length; i++) {
+            newList[i] = this.products[i];
+        }
+
+        newList[this.products.length] = product;
+
+        this.products = newList;
+    }
+}
