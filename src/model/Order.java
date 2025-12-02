acf23ac67ce570033a3eb1a93b3abd6dc33ad52c
Abdulmutee <abdulmoteesh@gmail.com>
2025-12-02 15:30:46 +0100

    Detta gjorde vi tillsammans

--- src/model/Order.java
+++ src/model/Order.java
@@ -1,65 +1,33 @@
 package model;
 
-import java.util.Random;
-
 public class Order {
 
-    private int id;
-    private int totalPrice;
     private Product[] products;
-    private Random random;
 
     public Order() {
         this.products = new Product[10];
-        random = new Random();
-        this.id = random.nextInt(1,100001);
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public int getTotalPrice() {
-        return totalPrice;
+    // Tillfälligt
+    public int totalPrice() {
+        return 0;
     }
 
     public void addProduct(Product product) {
-        for (int i = 0; i < this.products.length; i++) {
+        for (int i = 0; i< this.products.length; i++) {
             if (this.products[i] == null) {
                 this.products[i] = product;
                 return;
             }
         }
-        Product[] newList = new Product[this.products.length + 5];
+        Product[] newList = new Product[this.products.length+5];
 
-        for (int i = 0; i < this.products.length; i++) {
+        for (int i = 0; i< this.products.length; i++) {
             newList[i] = this.products[i];
         }
 
         newList[this.products.length] = product;
 
         this.products = newList;
-    }
-
-    public String[] getProductsAsString() {
-        String[] productsString = new String[products.length];
-
-        for (int i = 0; i < this.products.length; i++) {
-            if (products[i]!=null){
-                productsString[i] = this.products[i].displayProducts();
-            }
-        }
-        return productsString;
-    }
-
-    public int calculatePrice(){
-        int summ = 0;
-        for (Product product : products){
-            if (product != null){
-                summ += product.getPrice();
-            }
-        }
-        this.totalPrice = summ;
-        return summ;
     }
 }
