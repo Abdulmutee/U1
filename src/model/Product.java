a676f208260cb8b7e4e3470e871d70d0c7b3dd69
Abdulmutee <abdulmoteesh@gmail.com>
2025-12-02 14:59:30 +0100

    Detta gjorde vi tillsammans

--- src/model/Product.java
+++ src/model/Product.java
@@ -1,8 +1,10 @@
 package model;
 
-public abstract class Products {
+public abstract class Product {
     protected String name;
     protected int price;
 
     public abstract String displayProducts();
+
+    public abstract int getPrice();
 }
